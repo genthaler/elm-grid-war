438aa0ecba3948af67be13821c4a3089b96da2dc
Günther Enthaler <genthaler@users.noreply.github.com>
2019-03-01 07:04:39 +1100

    WIP

--- src/Model.elm
+++ src/Model.elm
@@ -1,7 +1,7 @@
 module Model exposing (Cell, Character(..), Model, Msg(..), Team(..), Terrain(..), encode)
 
 import Dict exposing (Dict)
-import Hexagons.Map exposing (Hash, Map)
+import Hexagons.Map exposing (Hash, Map, rectangularPointyTopMap)
 import Json.Decode as D
 import Json.Encode as E
 import Json.Encode.Extra as EExtra
@@ -74,10 +74,34 @@ toStringTerrain terrain =
             "Forest"
 
 
+fromStringTerrain string =
+    case string of
+        "Grass" ->
+            Grass
+
+        "Rock" ->
+            Rock
+
+        "Mountain" ->
+            Mountain
+
+        "Water" ->
+            Water
+
+        "Forest" ->
+            Forest
+
+
 toStringCharacter character =
     case character of
         Peasant ->
             "Peasant"
+
+
+fromStringCharacter character =
+    case character of
+        "Peasant" ->
+            Peasant
 
 
 toStringTeam team =
@@ -87,6 +111,15 @@ toStringTeam team =
 
         AI ->
             "AI"
+
+
+fromStringTeam team =
+    case team of
+        "Human" ->
+            Human
+
+        "AI" ->
+            AI
 
 
 encode : Int -> Model -> String
@@ -111,14 +144,43 @@ encode level model =
             ]
 
 
-
-decode : String -> Model
+decode : String -> Result D.Error Model
 decode =
     let
-        decodeModel = 
-          D.map4 Model
-                (D.field "cells" D.string)
-                (D.field "height" D.float)
-                (D.field "width" D.float)
+        decodeMap : D.Decoder Map
+        decodeMap =
+            D.map2 rectangularPointyTopMap
+                (D.field "height" D.int)
+                (D.field "width" D.int)
 
+        decodeCell : D.Decoder Cell
+        decodeCell =
+            D.map3 Cell
+                (D.field "terrain" (D.map fromStringTerrain D.string))
+                (D.field "character" (D.map fromStringCharacter D.string))
+                (D.field "team" (D.map fromStringTeam D.string))
+
+        triple : D.Decoder (List a)
+        triple list =
+            case list of
+                [ a, b, c ] ->
+                    succeed ( a, b, c )
+
+                _ ->
+                    fail ""
+
+        decodeHash : D.Decoder Hash
+        decodeHash =
+            D.list D.int
+                |> D.andThen
+
+        decodeModel : D.Decoder Model
+        decodeModel =
+            D.map5 Model
+                decodeMap
+                (D.field "cells" decodeCell)
+                (D.field "selectedCell" D.string)
+                (D.field "height" D.int)
+                (D.field "width" D.int)
     in
+    D.decodeString decodeModel
